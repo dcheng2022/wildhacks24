bf2de9c718260608c16c1a30edfef5409ba0d139
Eliseu <eliseuantonio.klosterfilho@gmail.com>
2024-04-06 16:21:49 -0500

    Larger field labels

--- mathexpress/src/ui/components/App.jsx
+++ mathexpress/src/ui/components/App.jsx
@@ -19,14 +19,14 @@ const App = ({ addOnUISdk, sandboxProxy }) => {
         // You may use "addOnUISdk.app.ui.theme" to get the current theme and react accordingly.
         <Theme theme="express" scale="medium" color="light">
 			<div className="container">
-				<FieldLabel for="latex" size="m">
+				<FieldLabel for="latex" size="l">
 					LaTeX
 				</FieldLabel>
-				<Textfield id="latex" size="m" placeholder="Enter LaTeX equation">
+				<Textfield id="latex" size="m" placeholder="Enter LaTeX expression">
 				</Textfield>
 			</div>
 			<div className="container">
-				<FieldLabel for="slider" size="m">
+				<FieldLabel for="slider" size="l">
 					Font Size
 				</FieldLabel>
 				<Slider editable>
