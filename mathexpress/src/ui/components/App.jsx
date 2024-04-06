ffa84d4feee3355ea327b8ada90eee0327623a20
dcheng <102439573+dcheng2022@users.noreply.github.com>
2024-04-06 16:06:25 -0500

    add LaTeX text field and label

--- mathexpress/src/ui/components/App.jsx
+++ mathexpress/src/ui/components/App.jsx
@@ -7,23 +7,23 @@ import "@spectrum-web-components/theme/express/theme-light.js";
 // https://opensource.adobe.com/spectrum-web-components/using-swc-react/
 import { Button } from "@swc-react/button";
 import { Theme } from "@swc-react/theme";
+import { FieldLabel } from "@swc-react/field-label";
+import { Textfield } from "@swc-react/textfield";
 import React from "react";
 import "./App.css";
 
 const App = ({ addOnUISdk, sandboxProxy }) => {
-    function handleClick() {
-        sandboxProxy.createRectangle();
-    }
-
     return (
         // Please note that the below "<Theme>" component does not react to theme changes in Express.
         // You may use "addOnUISdk.app.ui.theme" to get the current theme and react accordingly.
         <Theme theme="express" scale="medium" color="light">
-            <div className="container">
-                <Button size="m" onClick={handleClick}>
-                    Create Rectangle
-                </Button>
-            </div>
+			<div className="container">
+				<FieldLabel for="latex" size="m">
+					LaTeX
+				</FieldLabel>
+				<Textfield id="latex" size="m" placeholder="Enter LaTeX equation">
+				</Textfield>
+			</div>
         </Theme>
     );
 };
